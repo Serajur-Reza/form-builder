698789e421a180960de7b9a34bf5cbdae51f7d76
unknown <rezainfinity54@gmail.com>
2025-02-22 20:37:56 +0600

    project structured

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="font-bold text-center mt-4">
+        <h1 className="text-3xl">React Form Builder</h1>
+        <h3 className="text-xl mt-4">
+          Build forms without coding for your react application. Just drag and
+          drop a field to create a form for your react application.
+        </h3>
+      </div>
+    </>
+  );
+}
+
+export default App;
